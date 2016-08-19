19c1f1d6bd337d0b8e851d91cfa66924731c1e69
Martin Surynek <jakethashi@post.cz>
2016-08-19 11:18:51 +0200

    simple ajax test

--- app/app.ts
+++ app/app.ts
@@ -15,6 +15,61 @@ export class Test {
 
 _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 
-let tmp = _.template('<h1>{{ data }}</h1>');
+let tmp = _.template('<input id="text" type="text"></input><div id="resp" />');
 
-document.querySelector('#content').innerHTML = tmp({ data: new User('John').getName() }); 
+document.querySelector('#content').innerHTML = tmp({ data: new User('John').getName() });
+var respEl = document.querySelector('#resp');
+
+
+var req = (search, onResponse = (resp)=> {} ) => {
+    var http = new XMLHttpRequest();
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=cs&dt=t&q=" + search;
+    http.open("GET", url, true);
+    
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            try {
+                var response = JSON.parse(http.response.replace(/,+/g, ','))
+                onResponse(response);
+            }catch(e) {}
+        }
+    }
+    http.send();
+
+}
+
+
+var exec = _.debounce(search => {
+    req(search, resp => { 
+        console.log(resp);
+        respEl.innerHTML = resp[0][0][0];
+    });
+} , 500); 
+
+document.querySelector('#text').addEventListener('input', e => {
+    
+    var value = (<any>e.target).value;
+
+    if (value.length > 1) {
+        exec(value);
+    }
+
+    
+    // function r(value) {
+    //     setTimeout(() => {
+    //             //console.log(value);
+    //             if (value.length > 1) {
+    //                 req(value, resp => { 
+    //                     //console.log(resp.split("\"")[1]);
+    //                     respEl.innerHTML = resp.split("\"")[1];
+    //                 });
+    //             }            
+    //     }, 1000);
+    // }
+
+    //r(value);
+});
+
